6a47d981fea7f3e5f9310420f7e716c330374ac6
Emmanuele Villa <emmanuele.villa89@gmail.com>
2026-06-17 07:15:52 +0200

    Enhance MdCWord composable with layout improvements and support for test tags

--- shared/src/commonMain/kotlin/com/shadowings/hierowriter/MdCWord.kt
+++ shared/src/commonMain/kotlin/com/shadowings/hierowriter/MdCWord.kt
@@ -1,14 +1,16 @@
 package com.shadowings.hierowriter
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
@@ -19,7 +21,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.IntrinsicMeasurable
+import androidx.compose.ui.layout.IntrinsicMeasureScope
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.LayoutModifier
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Dp
@@ -27,7 +37,6 @@ import androidx.compose.ui.unit.dp
 import com.shadowings.hierowriter.HieroRepository.resFromGardinerCode
 import com.shadowings.hierowriter.ui.AppTheme
 import com.shadowings.hierowriter.ui.hieroglyphTint
-import com.shadowings.hierowriter.ui.neutralBlack
 import org.jetbrains.compose.resources.painterResource
 
 @Preview
